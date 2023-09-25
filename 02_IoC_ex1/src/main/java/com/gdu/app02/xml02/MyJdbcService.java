00fd4a048a6881a6e3bc919dc464c48e31e4bdcb
leemj98 <lmj98217@gmail.com>
2023-09-25 17:39:25 +0900

    230925

---
+++ 02_IoC_ex1/src/main/java/com/gdu/app02/xml02/MyJdbcService.java
@@ -0,0 +1,14 @@
+package com.gdu.app02.xml02;
+
+public class MyJdbcService {
+
+  private MyJdbcDao myjdbcDao;
+  
+  public void add() {
+    myjdbcDao.add();
+  }
+  public void remove() {
+    
+  }
+}
+
