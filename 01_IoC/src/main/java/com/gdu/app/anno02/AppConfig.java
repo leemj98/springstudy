acfc3f60b636abeca9df527724be3914af2e28ce
leemj98 <lmj98217@gmail.com>
2023-09-26 17:38:43 +0900

    230926

---
+++ 01_IoC/src/main/java/com/gdu/app/anno02/AppConfig.java
@@ -0,0 +1,10 @@
+package com.gdu.app.anno02;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class AppConfig {
+
+}
---
+++ 01_IoC/src/main/java/com/gdu/app/anno02/MainWrapper.java
@@ -0,0 +1,9 @@
+package com.gdu.app.anno02;
+
+public class MainWrapper {
+
+  public static void main(String[] args) {
+    // 
+  }
+  
+}
